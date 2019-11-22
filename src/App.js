8b23d843bf847d63a1c4ed29ba452bd6a7326402
Goose39 <Goose39.Dev@gmail.com>
2019-11-22 14:21:50 -0500

    Tabs practice Ex

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import './App.css';
 // import Counter from './state/Counter';
 // import HelloWorld from './state-drills/HelloWorld';
 // import Bomb from './state-drills/Bomb'
- import RouletteGun from './state-drills/RouletteGun';
+// import RouletteGun from './state-drills/RouletteGun';
+import Tabs from './state/Tabs';
 
 
 
@@ -25,12 +26,21 @@ import './App.css';
 // )
 
 
+const tabsProp = [
+  { name: 'First tab',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
+  { name: 'Second tab',
+    content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+  { name: 'Third tab',
+    content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
+];
+
 
 class App extends Component {
   render() {
     return (
       <main className='App'>
-        <RouletteGun />
+        <Tabs tabs={tabsProp}/>
        {/* <Bomb />
         <HelloWorld />
         <Counter count={123} step={2} />
